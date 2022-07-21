17935bbb7a4a009197fbedde59f44ce4c6f33168
Dima <raff2004bd@gmail.com>
2022-07-21 22:38:59 +0300

    add function askName

---
+++ src/cli.js
@@ -0,0 +1,13 @@
+import readlineSync from 'readline-sync';
+
+function askName(){
+
+console.log("Hello to the Brain Games!");
+const name = readlineSync.question('May I have your name?');
+
+console.log(`Hello, ${name}!`);
+
+return name;
+}
+
+export default askName;

2f64e244346fe6ea22187614d738827f8dea8148
Dima <raff2004bd@gmail.com>
2022-07-26 21:18:58 +0300

    update nod.js

--- src/games/nod.js
+++ src/games/nod.js
@@ -1,22 +1,24 @@
 import askName from '../cli.js';
-import commonLogic from "../index.js";
+import commonLogic from '../index.js';
 
-function randNumber(){
-    return Math.round(Math.random()*100);
+function randNumber() {
+  return Math.round(Math.random() * 100);
 }
 
-function brainNod(){
-    let numb1 = randNumber();
-    let numb2 = randNumber();
-    let question = numb1 + " " + numb2;
-    while (numb1 && numb2){numb1 > numb2 ? numb1%=numb2 : numb2%=numb1;}
-    numb1+=numb2;
-    return {"question": question,
-    "answer": numb1};
+function brainNod() {
+  let numb1 = randNumber();
+  let numb2 = randNumber();
+  const question = `${numb1} ${numb2}`;
+  while (numb1 && numb2) { numb1 > numb2 ? numb1 %= numb2 : numb2 %= numb1; }
+  numb1 += numb2;
+  return {
+    question,
+    answer: numb1,
+  };
 }
 
-function resNod()
-    let name = askName();
-    commonLogic(brainNod, name);
+function resNod() {
+  const name = askName();
+  commonLogic(brainNod, name);
 }
 export default resNod;
